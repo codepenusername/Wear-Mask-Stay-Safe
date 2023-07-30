020bc036409e8d7234d9ea0600c65c86e8aa6af5
Ayush <100988794+codepenusername@users.noreply.github.com>
2023-07-30 16:32:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+prediction_1 = ""
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JiCnMynGt/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("status").innerHTML = results[0].label;
+        prediction_1 = results[0].label;
+
+        if (results[0].label == "Entry Accepted") {
+            document.getElementById("update_emoji").innerHTML = "&#x1F637;";
+        }
+        if (results[0].label == "Entry Denied") {
+            document.getElementById("update_emoji").innerHTML = "&#x26d4;";
+        }
+    }
+}
